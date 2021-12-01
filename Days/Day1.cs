e38af2d8c218cf68c7f857bc793d5e97e305b7e7
gustavjunedahl <gustav.junedahl@gmail.com>
2021-12-01 12:17:20 +0100

    Day1

--- Days/Day1.cs
+++ Days/Day1.cs
@@ -6,14 +6,32 @@ namespace AoC2021.Days
 {
     public class Day1 : BaseDay
     {
+        private int largerThanPreviousNthItem(int[] depthMeasurements, int n)
+        {
+            var largerThanPreviousCount = 0;
+            for (int i = 0; i < depthMeasurements.Length - n; i++)
+            {
+                if (depthMeasurements[i] < depthMeasurements[i + n])
+                {
+                    largerThanPreviousCount++;
+                }
+            }
+            return largerThanPreviousCount;
+        }
         public override string PartOne(string input)
         {
-            return input;
+            var depthMeasurements = input.Split(Environment.NewLine).Select(int.Parse).ToArray();
+            return largerThanPreviousNthItem(depthMeasurements, 1).ToString();
         }
 
         public override string PartTwo(string input)
         {
-            throw new System.NotImplementedException();
+            var depthMeasurements = input.Split(Environment.NewLine).Select(int.Parse).ToArray();
+            /* The sliding window is 3 elements long where the next window 
+            * contains the last 2 elements of the previous window.
+            * Therefore we only need to compare the first and last element.
+            */
+            return largerThanPreviousNthItem(depthMeasurements, 3).ToString();
         }
     }
 }
